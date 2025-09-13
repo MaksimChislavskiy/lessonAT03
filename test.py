f9478e18d5193786ddc5ca8fecd68644f98927cf
MaksimChislavskiy <maxwell884884@gmail.com>
2025-09-13 17:10:21 +0300

    final

--- test.py
+++ test.py
@@ -7,11 +7,11 @@ def test_get_random_cat_image(mocker):
     mock_get.return_value.json.return_value = {
         'images': 'glkjhjh',
     }
-    data = get_random_cat_image()
-    assert data == {'images': 'glkjhjh'}
+    result = get_random_cat_image()
+    assert result == {'images': 'glkjhjh'}
 
 def test_get_random_cat_image_with_error(mocker):
    mock_get = mocker.patch('main.requests.get')
-   mock_get.return_value.status_code = 500
-   data = get_random_cat_image()
-   assert data == None
+   mock_get.return_value.status_code = 404
+   result = get_random_cat_image()
+   assert result == None
